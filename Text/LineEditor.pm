a42db7d2d286cd401b0fddf39d789db95c46942a
Ricky Buchanan <rb@tertius.net.au>
1998-08-21 10:26:46 +0000

    Import of GOSSAMER/Text-LineEitor-0.01 from CPAN.
    gitpan-cpan-distribution: Text-LineEitor
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         GOSSAMER/Text-LineEitor-0.01.tar.gz
    gitpan-cpan-author:       GOSSAMER
    gitpan-cpan-maturity:     released

---
+++ Text/LineEditor.pm
@@ -0,0 +1,106 @@
+package Text::LineEditor;
+#
+#
+#
+# Last updated by gossamer on Wed Aug 12 13:29:40 EST 1998
+#
+
+use strict;
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
+
+require Exporter;
+
+use Term::ReadLine;
+
+@ISA = qw(Exporter);
+@EXPORT = qw( line_editor );
+@EXPORT_OK = qw();
+
+$VERSION = "0.01";
+
+#
+# Constants
+#
+
+# When starting editing
+my $Initial_Prompt = 
+   "Enter your message: ('.' by itself on a line to end, ~h for help)";
+
+# Before each line is input
+my $Line_Prompt = "> ";
+
+# Before editor messages
+my $Message_Prompt = "*** ";
+
+my $Tempfile_Name = "/tmp/lineeditor.$$";
+
+#
+# Function
+#
+
+sub find_editor {
+   return $ENV{"visual"} || "vim" || "vi";
+}
+
+sub line_editor {
+   my $text;
+
+   my $finished = 0;
+
+   my $Input = new Term::ReadLine 'LineEditor';
+
+   print $Initial_Prompt . "\n";
+   while (!$finished) {
+
+      my $line = $Input->readline($Line_Prompt);  # NB  readline() removes \n
+
+      if ($line =~ m/^\.$/) {
+         # dot by itself - end
+         $finished++;
+
+      } elsif ($line =~ m/^~(.)(.*)$/) {
+         # Something magic
+
+         if ($1 eq 'w') {
+            # write to a file
+            open(OUTFILE, ">$2");
+            print OUTFILE $text;
+            close(OUTFILE);
+         } elsif (($1 eq 'e') || ($1 eq 'v')) {
+            # visual editor
+            my $editor = &find_editor;
+            if ($editor) {
+               open(OUTFILE, ">$Tempfile_Name");
+               print OUTFILE $text;
+               close(OUTFILE);
+
+               system($editor, $Tempfile_Name);
+
+               open(OUTFILE, "<$Tempfile_Name");
+               undef $/;
+               $text = <OUTFILE>;
+               close(OUTFILE);
+
+               print "DEBUG:  '$text'\n";
+            } else {
+               print $Message_Prompt . "Can't find an editor to use!\n";
+            }
+
+         } else {
+            print $Message_Prompt . "Unknown tilde escape '$line'\n";
+         }
+
+      } else {
+         # regular line
+
+         $text .= $line . "\n"; 
+      }
+   }
+
+   return $text;
+}
+
+#
+# End.
+#
+1;
---
+++ example.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+#
+#
+#
+# Last updated by gossamer on Wed Aug 12 20:10:06 EST 1998
+#
+
+
+use Text::LineEditor;
+
+my $text = line_editor();
+
+print "You entered:\n---\n";
+print $text;
+print "---\n";
+
+
